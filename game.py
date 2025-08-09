52f5034df98c0d715537047cf9e181aef9c1d5c8
songoku776 <craftandart7@gamil.com>
2025-08-09 13:09:36 +0600

    space invator p1

--- game.py
+++ game.py
@@ -1,21 +1,23 @@
-import math 
-import random
 import pygame
+import random
+import math
 
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 500
-PLAYER_START_X = 400
-PLAYER_START_Y = 380
+SCREEN_HEIGHT = 600
+
+PLAYER_START_X = 370
+PLAYER_START_Y = 480
+PLAYER_SPEED_X = 5
+
 ENEMY_START_Y_MIN = 50
 ENEMY_START_Y_MAX = 150
-ENEMY_SPEED_X = 4
-ENEMY_SPEED_Y = 40
-BULLET_SPEED_Y = 10
+ENEMY_SPEED_X = 3
+
 COLLISION_DISTANCE = 27
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Space Shooter")
+pygame.display.set_caption("Space Invader Project - Part 1")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
@@ -28,43 +30,76 @@ enemyImg = []
 enemyX = []
 enemyY = []
 enemyX_change = []
-enemyY_change = []
-num_of_enemies = 6
+num_of_enemies = 7
 
 for i in range(num_of_enemies):
     enemyImg.append(pygame.image.load('enemy.png'))
     enemyX.append(random.randint(0, SCREEN_WIDTH - 64))
     enemyY.append(random.randint(ENEMY_START_Y_MIN, ENEMY_START_Y_MAX))
     enemyX_change.append(ENEMY_SPEED_X)
-    enemyY_change.append(ENEMY_SPEED_Y)
-
-
-bulletImg = pygame.image.load('bullet.png')
-bulletX = 0
-bulletY = PLAYER_START_Y
-bulletY_change = BULLET_SPEED_Y
-bullet_state = "ready"
 
 score_value = 0
-font = pygame.font.Font('freesansbold.ttf',32)
+font = pygame.font.Font('freesansbold.ttf', 32)
 textX = 10
 textY = 10
-over_font = pygame.font.Font('freesansbold.ttf',64)
 
-def show_score(x,y):
-    score = font.render("Score:" + str(score_value),True,(255, 255, 255))
-    screen.blit(score, (x,y)) 
-def game_over_text():
-    over_text = over_font.render("GAME OVER",True,(255, 255, 255))
-    screen.blit(over_text, (SCREEN_WIDTH // 2 - 200, SCREEN_HEIGHT // 2 - 32))
+def show_score(x, y):
+    score = font.render("Score : " + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
+
 def player(x, y):
-    screen.blit(playerImg,(x,y))
+    screen.blit(playerImg, (x, y))
+
 def enemy(x, y, i):
-    screen.blit(enemyImg[i],(x,y))
-def fire_bullet(x, y):
-    global bullet_state
-    bullet_state ="fire"
-    screen.blit(bulletImg, (x + 16,y + 10))
-def is_collision(enemyX,enemyY,bulletX,bulletY):
-    distance = math.sqrt((math.pow(enemyX - bulletX,2)) + (math.pow(enemyY - bulletY,2)))
+    screen.blit(enemyImg[i], (x, y))
+
+def is_collision(enemyX, enemyY, playerX, playerY):
+    distance = math.sqrt((math.pow(enemyX - playerX, 2)) + (math.pow(enemyY - playerY, 2)))
     return distance < COLLISION_DISTANCE
+
+running = True
+clock = pygame.time.Clock()
+
+while running:
+    screen.fill((0, 0, 0))  
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -PLAYER_SPEED_X
+            if event.key == pygame.K_RIGHT:
+                playerX_change = PLAYER_SPEED_X
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= SCREEN_WIDTH - 64:
+        playerX = SCREEN_WIDTH - 64
+
+    for i in range(num_of_enemies):
+        enemyX[i] += enemyX_change[i]
+        if enemyX[i] <= 0:
+            enemyX_change[i] = ENEMY_SPEED_X
+        elif enemyX[i] >= SCREEN_WIDTH - 64:
+            enemyX_change[i] = -ENEMY_SPEED_X
+
+        if is_collision(enemyX[i], enemyY[i], playerX, playerY):
+            score_value += 1
+            enemyX[i] = random.randint(0, SCREEN_WIDTH - 64)
+            enemyY[i] = random.randint(ENEMY_START_Y_MIN, ENEMY_START_Y_MAX)
+
+        enemy(enemyX[i], enemyY[i], i)
+
+    player(playerX, playerY)
+    show_score(textX, textY)
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
